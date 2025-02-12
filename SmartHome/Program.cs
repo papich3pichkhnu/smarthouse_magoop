762ffeb4de5c2dd912a8cf09194c157dda5b544e
papich3pichkhnu <90156180+papich3pichkhnu@users.noreply.github.com>
2025-02-12 10:28:29 +0200

    create project

---
+++ SmartHome/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SmartHome{
+    public class MainProgram{
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, Smart Home");
+        }
+        
+    }
+}
